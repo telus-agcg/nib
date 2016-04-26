1d5f7f377238bd87f771a23b96317e06a37e4701
John Allen <john@threedogconsulting.com>
2016-04-26 11:38:42 -0400

    Tag current commit as latest during releaes
    This allows for the "latest" tag on the Docker hub to reflect the most
    recently released commit.

--- release.sh
+++ release.sh
@@ -1,5 +1,14 @@
 #! /bin/bash
 
+# delete local "latest" tag
+git tag -d latest
+
+# delete remote "latest" tag
+git push origin :refs/tags/latest
+
+# tag current commit with "latest"
+git tag latest
+
 # tag with version from VERSION
 git tag $(cat VERSION)
 
