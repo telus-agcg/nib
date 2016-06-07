668c9d37cd53adfacb27587b13ae56c07b72dd3a
John Allen <john@threedogconsulting.com>
2016-06-07 17:50:30 -0400

    Add sanity checks to release script

--- release.sh
+++ release.sh
@@ -1,5 +1,41 @@
 #! /bin/bash
 
+# update commands document
+docker run \
+  --rm \
+  -v $PWD:/usr/src/app \
+  -w /usr/src/app \
+  --entrypoint=./update_docs.sh \
+  technekes/nib
+
+# fail release if commands changed (need to commit first)
+if [[ $(git status | grep docs/commands.md) ]]; then
+  echo "ERROR:
+
+The commands document (docs/commands.md) has been updated. \
+Please commit the changes and try again."
+
+  exit 1
+fi
+
+NIB_VERSION=$(cat VERSION)
+LATEST=$(curl --silent https://raw.githubusercontent.com/technekes/nib/latest/VERSION)
+
+if [[ $(git status | grep VERSION) ]]; then
+  echo "ERROR:
+
+The VERSION file has changed but not been committed. \
+Please commit the changes and try again."
+
+  exit 1
+elif [ "$NIB_VERSION" == "$LATEST" ]; then
+  echo "ERROR:
+
+Please update the VERSION file, it currently matches the previous release"
+
+  exit 1
+fi
+
 # delete local "latest" tag
 git tag -d latest
 
