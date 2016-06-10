a2d7ed7f4988e61730c3ff0d26b890f3e2258e12
John Allen <john@threedogconsulting.com>
2016-06-09 23:29:07 -0400

    Use config/boot.rb in console command if present
    The convention for Ruby apps is to use `./config/boot.rb` to require
    project files. If this is present then let's take advantage of it.

--- spec/localhost/console_spec.rb
+++ spec/localhost/console_spec.rb
@@ -1,9 +1,10 @@
 require 'pty'
+require 'expect'
 
 RSpec.describe 'console' do
   def tty
     PTY.spawn('cd spec/dummy && nibtest console web') do |stdout, stdin, pid|
-      stdout.eof?
+      stdin.sync = true
 
       yield(stdout, stdin)
 
@@ -16,15 +17,53 @@ RSpec.describe 'console' do
     end
   end
 
-  it 'starts a pry session and accepts input' do
-    tty do |stdout, stdin|
-      # ideally we do not have to write a message at all, we simply want to
-      # check for a prompt. however if we do not perform a #puts here then
-      # stdout.gets hangs indefinitely.
-      stdin.puts 'puts "foo"'
+  context 'script console' do
+    let(:script_file) { './spec/dummy/script/console' }
 
-      expect(stdout.gets).to match(/puts \"foo\"/)
-      expect(stdout.gets).to match(/pry\(main\)>/)
+    around(:each) do |example|
+      File.rename "#{script_file}.stub", script_file
+
+      example.run
+
+      File.rename script_file, "#{script_file}.stub"
+    end
+
+    it 'starts an irb session and accepts input' do
+      tty do |stdout, stdin|
+        stdout.expect(/irb/, 5) { stdin.puts 'puts "foo"' }
+
+        expect(stdout.gets).to match(/puts \"foo\"/)
+      end
+    end
+  end
+
+  context 'has pry' do
+    it 'starts a pry session and accepts input' do
+      tty do |stdout, stdin|
+        stdout.expect(/pry/, 5) { stdin.puts 'puts "foo"' }
+
+        expect(stdout.gets).to match(/puts \"foo\"/)
+      end
+    end
+  end
+
+  context 'has boot' do
+    let(:boot_file) { './spec/dummy/config/boot.rb' }
+
+    around(:each) do |example|
+      File.rename "#{boot_file}.stub", boot_file
+
+      example.run
+
+      File.rename boot_file, "#{boot_file}.stub"
+    end
+
+    it 'loads classes required by boot' do
+      tty do |stdout, stdin|
+        stdout.expect(/pry/, 5) { stdin.puts 'Foo' }
+
+        expect(stdout.gets).to match(/\(main\)> Foo/)
+      end
     end
   end
 end
