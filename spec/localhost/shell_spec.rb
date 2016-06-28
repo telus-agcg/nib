9c75f02eb060f649809838f0b0258ea9cef3544e
John Allen <john@threedogconsulting.com>
2016-06-28 17:29:40 -0400

    Add specs for `shell`
    Additional minor refactoring to re-use the `#tty` method previously
    defined in the `console` spec.

--- spec/localhost/shell_spec.rb
+++ spec/localhost/shell_spec.rb
@@ -1,3 +1,21 @@
-RSpec.describe 'shell' do
-  pending
+require 'pty'
+require 'expect'
+
+RSpec.describe 'shell', :interactive do
+  let(:command) { "cd #{spec_dir} && nibtest shell web" }
+
+  { rails: :bash, sinatra: :ash }.each do |app, shell|
+    context shell do
+      let(:spec_dir) { "./spec/dummy/#{app}" }
+
+      it 'starts an interactive bash session' do
+        tty(command) do |stdout, stdin|
+          stdout.expect(%r{\/usr\/src\/app}, 5) { stdin.puts 'echo $0' }
+
+          stdout.gets # why extra #gets necessary?
+          expect(stdout.gets).to match(/#{shell}/)
+        end
+      end
+    end
+  end
 end
