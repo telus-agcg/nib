4f14061d11edadb159ee1eeecf2d0aa63fc43924
John Allen <john@threedogconsulting.com>
2016-06-29 01:14:25 -0400

    Remove spec for obsolete yaml2json

--- spec/localhost/yaml2json_spec.rb
+++
@@ -1,6 +0,0 @@
-cmd = 'docker run --rm --entrypoint=ash nibtest -c "which yaml2json"'
-
-RSpec.describe command(cmd) do
-  its(:stdout) { should match(/\/usr\/local\/bin\/yaml2json/) }
-  its(:exit_status) { should eq 0 }
-end
