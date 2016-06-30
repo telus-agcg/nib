5cb3071c873f7aa37ea433035a24695bd3a16f01
John Allen <john@threedogconsulting.com>
2016-06-29 20:37:45 -0400

    Add spec for `debug`

--- spec/localhost/exec_spec.rb
+++ spec/localhost/exec_spec.rb
@@ -1,19 +1,4 @@
-RSpec.describe 'exec' do
-  around(:each) do |example|
-    `
-      cd spec/dummy/rails
-      docker-compose up -d
-    `
-
-    example.run
-
-    `
-      cd spec/dummy/rails
-      docker-compose stop
-      docker-compose rm --all -f
-    `
-  end
-
+RSpec.describe 'exec', :running_rails_server do
   describe command("cd spec/dummy/rails; nibtest exec web 'ps x'") do
     its(:stdout) { should match(%r{\/bin\/rackup\ -p}) }
     its(:exit_status) { should eq 0 }
---
+++ spec/support/contexts/running_rails_server.rb
@@ -0,0 +1,16 @@
+RSpec.shared_context 'running rails server', :running_rails_server do
+  around(:each) do |example|
+    `
+      cd spec/dummy/rails
+      docker-compose up -d
+    `
+
+    example.run
+
+    `
+      cd spec/dummy/rails
+      docker-compose stop
+      docker-compose rm --all -f
+    `
+  end
+end
