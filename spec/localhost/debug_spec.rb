846da5cae53be3a07b74b11abbe32d29b62664e5
John Allen <john@threedogconsulting.com>
2016-06-29 20:37:45 -0400

    Add spec for `debug`

--- spec/localhost/debug_spec.rb
+++ spec/localhost/debug_spec.rb
@@ -1,3 +1,14 @@
-RSpec.describe 'debug' do
-  pending
+require 'expect'
+
+RSpec.describe 'debug', :interactive, :running_rails_server do
+  let(:spec_dir) { './spec/dummy/rails' }
+  let(:command)  { "pwd && cd #{spec_dir} && nibtest debug web" }
+
+  it 'connects to a running debug server' do
+    tty(command) do |stdout, _|
+      output = stdout.expect(/Connected/, 5)
+
+      expect(output.first).to match(/Connecting to byebug server/)
+    end
+  end
 end
--- spec/support/intertactivity.rb
+++ spec/support/intertactivity.rb
@@ -1,12 +1,11 @@
 module Interactivity
-  def tty(command)
+  def tty(command, signal = 'SIGTERM')
     PTY.spawn(command) do |stdout, stdin, pid|
       stdin.sync = true
 
       yield(stdout, stdin)
 
-      stdin.puts 'exit'
-
+      Process.kill(signal, pid) rescue Errno::ESRCH
       Process.waitpid(pid, 0)
 
       stdin.close
