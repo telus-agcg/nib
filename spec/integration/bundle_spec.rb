85832dfeeb5687b5fb4054b4f07c495599515a52
John Allen <john@threedogconsulting.com>
2016-07-21 17:13:21 -0400

    Add codeship support
    Note: can only run unit tests until codeship supports compose v2

--- spec/integration/bundle_spec.rb
+++ spec/integration/bundle_spec.rb
@@ -1,3 +1,5 @@
+require 'serverspec_helper'
+
 RSpec.describe command('cd spec/dummy/rails && nibtest bundle web help') do
   its(:stdout) { should match(/BUNDLE\(1\)/) }
   its(:exit_status) { should eq 0 }
