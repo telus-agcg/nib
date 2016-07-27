0748ee41e51e9db2d7076ed4bdc1ebceda55dc00
John Allen <john@threedogconsulting.com>
2016-07-26 20:22:00 -0400

    Add support for --service-ports with `run` command
    Resolves #89

--- lib/nib/command.rb
+++ lib/nib/command.rb
@@ -1,21 +1,31 @@
 module Nib::Command
   def self.included(base)
     base.instance_eval do
-      attr_reader :service, :command
+      attr_reader :service, :command, :raw_options
 
       extend ClassMethods
     end
   end
 
   module ClassMethods
-    def execute(_, args)
-      new(args.shift, args.join(' ')).execute
+    def execute(raw_options, args)
+      # - first argument is (almost) always the service
+      # - pass the remaining arguments along as command
+      # - gli sends a string and symbol version of options, reduce to string
+      #   > { 'enabled' => true, :enabled => true }.map { |k, v| [k.to_s, v] }
+      #   => { 'enabled' => true }
+      new(
+        args.shift,
+        args.join(' '),
+        raw_options.map { |k, v| [k.to_s, v] }.to_h
+      ).execute
     end
   end
 
-  def initialize(service, command)
+  def initialize(service, command, raw_options = {})
     @service = service
     @command = command
+    @raw_options = raw_options
   end
 
   def execute
@@ -27,8 +37,19 @@ module Nib::Command
       docker-compose \
         run \
         --rm \
+        #{options} \
         #{service} \
         #{command}
     SCRIPT
+  end
+
+  private
+
+  def parsed_options
+    []
+  end
+
+  def options
+    parsed_options.join(' ')
   end
 end
